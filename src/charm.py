d739bc02b9d57673a9d01f81079cc722622bd383
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-11-11 18:07:13 +0100

    added charm configuration

--- src/charm.py
+++ src/charm.py
@@ -8,6 +8,9 @@ import logging
 from ops.charm import CharmBase
 from ops.main import main
 from ops.model import ActiveStatus
+from ops.model import BlockedStatus
+from ops.model import MaintenanceStatus
+from ops.model import WaitingStatus
 from ops.pebble import Layer
 
 # Log messages can be retrieved using juju debug-log
@@ -20,25 +23,48 @@ class FastAPIDemoCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.pebble_service_name = "fastapi-service"
-        self.framework.observe(self.on.demo_server_pebble_ready, self._on_demo_server_pebble_ready)
+        self.container = self.unit.get_container("demo-server")  # see 'containers' in metadata.yaml
+        self.framework.observe(self.on.demo_server_pebble_ready, self._update_layer_and_restart)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
 
-    def _on_demo_server_pebble_ready(self, event):
+    def _on_config_changed(self, event):
+        port = self.config["server-port"]  # see config.yaml
+        logger.debug("New application port is requested: %s", port)
+
+        if int(port) == 22:
+            self.unit.status = BlockedStatus("Invalid port number, 22 is reserved for SSH")
+            return
+
+        self._update_layer_and_restart(None)
+
+    def _update_layer_and_restart(self, event) -> None:
         """Define and start a workload using the Pebble API.
 
-        Change this example to suit your needs. You'll need to specify the right entrypoint and
-        environment configuration for your specific workload.
+        You'll need to specify the right entrypoint and environment
+        configuration for your specific workload. Tip: you can see the
+        standard entrypoint of an existing container using docker inspect
 
-        Learn more about interacting with Pebble at at https://juju.is/docs/sdk/pebble.
+        Learn more about Pebble layers at https://github.com/canonical/pebble
         """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Add initial Pebble config layer using the Pebble API
-        container.add_layer("fastapi_demo", self._pebble_layer, combine=True)
-        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
-        container.replan()
+
         # Learn more about statuses in the SDK docs:
         # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
+        self.unit.status = MaintenanceStatus("Assembling pod spec")
+        if self.container.can_connect():
+            new_layer = self._pebble_layer.to_dict()
+            # Get the current pebble layer config
+            services = self.container.get_plan().to_dict().get("services", {})
+            if services != new_layer["services"]:
+                # Changes were made, add the new layer
+                self.container.add_layer("fastapi_demo", self._pebble_layer, combine=True)
+                logger.info("Added updated layer 'fastapi_demo' to Pebble plan")
+
+                self.container.restart(self.pebble_service_name)
+                logger.info(f"Restarted '{self.pebble_service_name}' service")
+
+            self.unit.status = ActiveStatus()
+        else:
+            self.unit.status = WaitingStatus("Waiting for Pebble in workload container")
 
     @property
     def _pebble_layer(self):
@@ -48,7 +74,7 @@ class FastAPIDemoCharm(CharmBase):
                 "uvicorn",
                 "api_demo_server.app:app",
                 "--host=0.0.0.0",
-                "--port=8000",
+                f"--port={self.config['server-port']}",
             ]
         )
         pebble_layer = {

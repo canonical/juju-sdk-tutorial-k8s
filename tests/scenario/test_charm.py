601981c329e55bc3f1a83e0d790fb9db2baa4119
Benjamin Schimke <benjamin.schimke@canonical.com>
2023-10-12 14:46:47 +0200

    Add scenario based test

---
+++ tests/scenario/test_charm.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+from charm import FastAPIDemoCharm
+from scenario import Relation, State, Context, Container, Action
+
+import unittest
+import unittest.mock
+
+
+class TestCharm(unittest.TestCase):
+    @unittest.mock.patch("charm.LogProxyConsumer")
+    @unittest.mock.patch("charm.MetricsEndpointProvider")
+    @unittest.mock.patch("charm.GrafanaDashboardProvider")
+    def test_get_db_info_action(self, *_):
+        # use scenario.Context to declare what charm we are testing
+        ctx = Context(
+            FastAPIDemoCharm,
+            meta={
+                "name": "demo-api-charm",
+                "containers": {"demo-server": {}},
+                "peers": {"fastapi-peer": {"interface": "fastapi_demo_peers"}},
+                "requires": {
+                    "database": {
+                    "interface": "postgresql_client",
+                    }
+                },
+            },
+            config={
+                "options": {
+                    "server-port": {
+                    "default": 8000,
+                    }
+                }
+            },
+            actions={
+                "get-db-info": {
+                    "params": {"show-password": {"default": False, "type": "boolean"}}
+                }
+            },
+        )
+
+        # declare the input state
+        state_in = State(
+            leader=True,
+            relations=[
+                Relation(
+                    endpoint="database",
+                    interface="postgresql_client",
+                    remote_app_name="postgresql-k8s",
+                    local_unit_data={},
+                    remote_app_data={
+                        "endpoints": "127.0.0.1:5432",
+                        "username": "foo",
+                        "password": "bar",
+                    },
+                ),
+            ],
+            containers=[
+                Container(name="demo-server", can_connect=True),
+            ],
+        )
+
+        #run the action with the defined state and collect the output.
+        action = Action("get-db-info", {"show-password": True})
+        action_out = ctx.run_action(action, state_in)
+
+        assert action_out.results == {
+            "db-host": "127.0.0.1",
+            "db-port": "5432",
+            "db-username": "foo",
+            "db-password": "bar",
+        }

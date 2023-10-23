1e05ca7d3dc0a905907484c7bbb78b9f518fc1cf
IbraAoad <ibrahim.awwad@canonical.com>
2023-10-23 14:31:31 +0300

    Adding integration tests for open port

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -10,6 +10,8 @@ import pytest
 import yaml
 from pytest_operator.plugin import OpsTest
 
+from helpers import is_port_open, get_address
+
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
@@ -55,3 +57,30 @@ async def test_database_integration(ops_test: OpsTest):
     await ops_test.model.wait_for_idle(
         apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
     )
+
+@pytest.mark.abort_on_fail
+async def test_open_ports(ops_test: OpsTest):
+    """Verify that setting the server-port in charm's config correctly adjust k8s service
+
+    Assert blocked status in case of port 22 and active status for others
+    """
+    app = ops_test.model.applications.get("demo-api-charm")
+
+    # Get the k8s service address of the app
+    address = await get_address(ops_test=ops_test, app_name=APP_NAME)
+    # Validate that initial port is opened
+    assert is_port_open(address, 8000)
+
+    # Set Port to 22 and validate app going to blocked status with port not opened
+    await app.set_config({"server-port": "22"})
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME], status="blocked", timeout=1000
+    ),
+    assert not is_port_open(address, 22)
+
+    # Set Port to 6789 "Dummy port" and validate app going to active status with port opened
+    await app.set_config({"server-port": "6789"})
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME], status="active", timeout=1000
+    ),
+    assert is_port_open(address, 6789)

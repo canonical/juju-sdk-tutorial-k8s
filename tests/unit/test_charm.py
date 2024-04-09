6e9f75010d5790d737c69f1cfaaa0881f51642e3
Tony Meyer <tony.meyer@canonical.com>
2024-04-09 12:46:13 +1200

    Modernisation.

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,11 +3,11 @@
 # See LICENSE file for licensing details.
 #
 # Learn more at: https://juju.is/docs/sdk
-from charm import FastAPIDemoCharm
-
 import ops
 import ops.testing
 import pytest
+
+from charm import FastAPIDemoCharm
 
 
 @pytest.fixture
@@ -20,7 +20,6 @@ def harness():
 
 def test_pebble_layer(monkeypatch, harness: ops.testing.Harness[FastAPIDemoCharm]):
     monkeypatch.setattr(FastAPIDemoCharm, "version", "1.0.0")
-    monkeypatch.setattr(FastAPIDemoCharm, "fetch_postgres_relation_data", lambda *_: {})
     # Expected plan after Pebble ready with default config
     expected_plan = {
         "services": {
@@ -29,27 +28,23 @@ def test_pebble_layer(monkeypatch, harness: ops.testing.Harness[FastAPIDemoCharm
                 "summary": "fastapi demo",
                 "command": "uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000",
                 "startup": "enabled",
-                "environment": {
-                    "DEMO_SERVER_DB_HOST": None,
-                    "DEMO_SERVER_DB_PASSWORD": None,
-                    "DEMO_SERVER_DB_USER": None,
-                    "DEMO_SERVER_DB_PORT": None,
-                },
+                # Since the environment is empty, Layer.to_dict() will not
+                # include it.
             }
         }
     }
 
     # Simulate the container coming up and emission of pebble-ready event
     harness.container_pebble_ready("demo-server")
+    harness.evaluate_status()
+
     # Get the plan now we've run PebbleReady
     updated_plan = harness.get_container_pebble_plan("demo-server").to_dict()
-    service = harness.model.unit.get_container("demo-server").get_service(
-        "fastapi-service"
-    )
-
+    service = harness.model.unit.get_container("demo-server").get_service("fastapi-service")
     # Check that we have the plan we expected:
     assert updated_plan == expected_plan
     # Check the service was started:
     assert service.is_running()
-    # Ensure we set an ActiveStatus with no message:
-    assert harness.model.unit.status == ops.ActiveStatus()
+    # Ensure we set a BlockedStatus with appropriate message:
+    assert isinstance(harness.model.unit.status, ops.BlockedStatus)
+    assert "Waiting for database" in harness.model.unit.status.message

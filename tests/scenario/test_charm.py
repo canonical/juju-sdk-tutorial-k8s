4d3402d0f6fa16f884fa3fd5ad2f765828b68509
James Garner <james.garner@canonical.com>
2024-09-10 15:11:09 +1200

    Minor improvements and ruff formatting

--- tests/scenario/test_charm.py
+++ tests/scenario/test_charm.py
@@ -12,33 +12,32 @@ from charm import FastAPIDemoCharm
 
 
 def test_get_db_info_action(monkeypatch: MonkeyPatch):
-
-    monkeypatch.setattr("charm.LogProxyConsumer", Mock())
-    monkeypatch.setattr("charm.MetricsEndpointProvider", Mock())
-    monkeypatch.setattr("charm.GrafanaDashboardProvider", Mock())
+    monkeypatch.setattr('charm.LogProxyConsumer', Mock())
+    monkeypatch.setattr('charm.MetricsEndpointProvider', Mock())
+    monkeypatch.setattr('charm.GrafanaDashboardProvider', Mock())
 
     # Use scenario.Context to declare what charm we are testing.
     ctx = scenario.Context(
         FastAPIDemoCharm,
         meta={
-            "name": "demo-api-charm",
-            "containers": {"demo-server": {}},
-            "peers": {"fastapi-peer": {"interface": "fastapi_demo_peers"}},
-            "requires": {
-                "database": {
-                    "interface": "postgresql_client",
+            'name': 'demo-api-charm',
+            'containers': {'demo-server': {}},
+            'peers': {'fastapi-peer': {'interface': 'fastapi_demo_peers'}},
+            'requires': {
+                'database': {
+                    'interface': 'postgresql_client',
                 }
             },
         },
         config={
-            "options": {
-                "server-port": {
-                    "default": 8000,
+            'options': {
+                'server-port': {
+                    'default': 8000,
                 }
             }
         },
         actions={
-            "get-db-info": {"params": {"show-password": {"default": False, "type": "boolean"}}}
+            'get-db-info': {'params': {'show-password': {'default': False, 'type': 'boolean'}}}
         },
     )
 
@@ -47,29 +46,29 @@ def test_get_db_info_action(monkeypatch: MonkeyPatch):
         leader=True,
         relations=[
             scenario.Relation(
-                endpoint="database",
-                interface="postgresql_client",
-                remote_app_name="postgresql-k8s",
+                endpoint='database',
+                interface='postgresql_client',
+                remote_app_name='postgresql-k8s',
                 local_unit_data={},
                 remote_app_data={
-                    "endpoints": "127.0.0.1:5432",
-                    "username": "foo",
-                    "password": "bar",
+                    'endpoints': '127.0.0.1:5432',
+                    'username': 'foo',
+                    'password': 'bar',
                 },
             ),
         ],
         containers=[
-            scenario.Container(name="demo-server", can_connect=True),
+            scenario.Container(name='demo-server', can_connect=True),
         ],
     )
 
     # run the action with the defined state and collect the output.
-    action = scenario.Action("get-db-info", {"show-password": True})
+    action = scenario.Action('get-db-info', {'show-password': True})
     action_out = ctx.run_action(action, state_in)
 
     assert action_out.results == {
-        "db-host": "127.0.0.1",
-        "db-port": "5432",
-        "db-username": "foo",
-        "db-password": "bar",
+        'db-host': '127.0.0.1',
+        'db-port': '5432',
+        'db-username': 'foo',
+        'db-password': 'bar',
     }

2c168bd8744a86e8c0b90d71016d30f0196e28f6
James Garner <james.garner@canonical.com>
2024-09-10 15:07:57 +1200

    Minor improvements and ruff formatting

--- src/charm.py
+++ src/charm.py
@@ -18,11 +18,11 @@ from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
-PEER_NAME = "fastapi-peer"
+PEER_NAME = 'fastapi-peer'
 
 JSONData = Union[
-    Dict[str, "JSONData"],
-    List["JSONData"],
+    Dict[str, 'JSONData'],
+    List['JSONData'],
     str,
     int,
     float,
@@ -36,14 +36,16 @@ class FastAPIDemoCharm(ops.CharmBase):
 
     def __init__(self, framework: ops.Framework) -> None:
         super().__init__(framework)
-        self.pebble_service_name = "fastapi-service"
-        self.container = self.unit.get_container("demo-server")  # see 'containers' in charmcraft.yaml
-        framework.observe(self.on["demo-server"].pebble_ready, self._on_demo_server_pebble_ready)
+        self.pebble_service_name = 'fastapi-service'
+        self.container = self.unit.get_container(
+            'demo-server'
+        )  # see 'containers' in charmcraft.yaml
+        framework.observe(self.on.demo_server_pebble_ready, self._on_demo_server_pebble_ready)
         framework.observe(self.on.config_changed, self._on_config_changed)
         framework.observe(self.on.collect_unit_status, self._on_collect_status)
         framework.observe(self.on.start, self._count)
         # Charm events defined in the database requires charm library:
-        self.database = DatabaseRequires(self, relation_name="database", database_name="names_db")
+        self.database = DatabaseRequires(self, relation_name='database', database_name='names_db')
         framework.observe(self.database.on.database_created, self._on_database_created)
         framework.observe(self.database.on.endpoints_changed, self._on_database_created)
         # Events on custom actions that are run via 'juju run-action':
@@ -51,44 +53,46 @@ class FastAPIDemoCharm(ops.CharmBase):
         # Provide ability for prometheus to be scraped by Prometheus using prometheus_scrape
         self._prometheus_scraping = MetricsEndpointProvider(
             self,
-            relation_name="metrics-endpoint",
-            jobs=[{"static_configs": [{"targets": [f"*:{self.config['server-port']}"]}]}],
+            relation_name='metrics-endpoint',
+            jobs=[{'static_configs': [{'targets': [f"*:{self.config['server-port']}"]}]}],
             refresh_event=self.on.config_changed,
         )
         # Enable log forwarding for Loki and other charms that implement loki_push_api
         self._logging = LogProxyConsumer(
-            self, relation_name="log-proxy", log_files=["demo_server.log"]
+            self, relation_name='log-proxy', log_files=['demo_server.log']
         )
         # Provide grafana dashboards over a relation interface
-        self._grafana_dashboards = GrafanaDashboardProvider(self, relation_name="grafana-dashboard")
+        self._grafana_dashboards = GrafanaDashboardProvider(
+            self, relation_name='grafana-dashboard'
+        )
 
     def _on_collect_status(self, event: ops.CollectStatusEvent) -> None:
-        port = self.config["server-port"]
+        port = self.config['server-port']
         if port == 22:
-            event.add_status(ops.BlockedStatus("Invalid port number, 22 is reserved for SSH"))
-        if not self.model.get_relation("database"):
+            event.add_status(ops.BlockedStatus('Invalid port number, 22 is reserved for SSH'))
+        if not self.model.get_relation('database'):
             # We need the user to do 'juju integrate'.
-            event.add_status(ops.BlockedStatus("Waiting for database relation"))
+            event.add_status(ops.BlockedStatus('Waiting for database relation'))
         elif not self.database.fetch_relation_data():
             # We need the charms to finish integrating.
-            event.add_status(ops.WaitingStatus("Waiting for database relation"))
+            event.add_status(ops.WaitingStatus('Waiting for database relation'))
         try:
             status = self.container.get_service(self.pebble_service_name)
         except (ops.pebble.APIError, ops.pebble.ConnectionError, ops.ModelError):
-            event.add_status(ops.MaintenanceStatus("Waiting for Pebble in workload container"))
+            event.add_status(ops.MaintenanceStatus('Waiting for Pebble in workload container'))
         else:
             if not status.is_running():
-                event.add_status(ops.MaintenanceStatus("Waiting for the service to start up"))
+                event.add_status(ops.MaintenanceStatus('Waiting for the service to start up'))
         # If nothing is wrong, then the status is active.
         event.add_status(ops.ActiveStatus())
 
     def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
-        port = self.config["server-port"]  # see charmcraft.yaml
-        logger.debug("New application port is requested: %s", port)
+        port = self.config['server-port']  # see charmcraft.yaml
+        logger.debug('New application port is requested: %s', port)
 
         if port == 22:
             # The collect-status handler will set the status to blocked.
-            logger.info("Invalid port number, 22 is reserved for SSH")
+            logger.info('Invalid port number, 22 is reserved for SSH')
             return
 
         self._update_layer_and_restart()
@@ -99,9 +103,9 @@ class FastAPIDemoCharm(ops.CharmBase):
         It retrieves the current count of pod starts from the 'unit_stats' peer relation data,
         increments the count, and then updates the 'unit_stats' data with the new count.
         """
-        unit_stats = self.get_peer_data("unit_stats")
-        counter = cast(str, unit_stats.get("started_counter", "0"))
-        self.set_peer_data("unit_stats", {"started_counter": int(counter) + 1})
+        unit_stats = self.get_peer_data('unit_stats')
+        counter = cast(str, unit_stats.get('started_counter', '0'))
+        self.set_peer_data('unit_stats', {'started_counter': int(counter) + 1})
 
     def _on_demo_server_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
         self._update_layer_and_restart()
@@ -119,20 +123,20 @@ class FastAPIDemoCharm(ops.CharmBase):
 
         Learn more about actions at https://juju.is/docs/sdk/actions
         """
-        show_password = event.params["show-password"]  # see charmcraft.yaml
+        show_password = event.params['show-password']  # see charmcraft.yaml
         db_data = self.fetch_postgres_relation_data()
         if not db_data:
-            event.fail("No database connected")
+            event.fail('No database connected')
             return
         output = {
-            "db-host": db_data.get("db_host", None),
-            "db-port": db_data.get("db_port", None),
+            'db-host': db_data.get('db_host', None),
+            'db-port': db_data.get('db_port', None),
         }
         if show_password:
             output.update(
                 {
-                    "db-username": db_data.get("db_username", None),
-                    "db-password": db_data.get("db_password", None),
+                    'db-username': db_data.get('db_username', None),
+                    'db-password': db_data.get('db_password', None),
                 }
             )
         event.set_results(output)
@@ -145,24 +149,25 @@ class FastAPIDemoCharm(ops.CharmBase):
         standard entrypoint of an existing container using docker inspect
 
         Learn more about interacting with Pebble at https://juju.is/docs/sdk/pebble
-        Learn more about Pebble layers at https://github.com/canonical/pebble
+        Learn more about Pebble layers at
+            https://canonical-pebble.readthedocs-hosted.com/en/latest/reference/layers
         """
 
         # Learn more about statuses in the SDK docs:
         # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ops.MaintenanceStatus("Assembling Pebble layers")
+        self.unit.status = ops.MaintenanceStatus('Assembling Pebble layers')
         try:
             # Get the current pebble layer config
-            services = self.container.get_plan().to_dict().get("services", {})
-            if services != self._pebble_layer.to_dict().get("services", {}):
+            services = self.container.get_plan().to_dict().get('services', {})
+            if services != self._pebble_layer.to_dict().get('services', {}):
                 # Changes were made, add the new layer
-                self.container.add_layer("fastapi_demo", self._pebble_layer, combine=True)
+                self.container.add_layer('fastapi_demo', self._pebble_layer, combine=True)
                 logger.info("Added updated layer 'fastapi_demo' to Pebble plan")
 
                 self.container.restart(self.pebble_service_name)
                 logger.info(f"Restarted '{self.pebble_service_name}' service")
         except (ops.pebble.APIError, ops.pebble.ConnectionError) as e:
-            logger.info("Unable to connect to Pebble: %s", e)
+            logger.info('Unable to connect to Pebble: %s', e)
             return
         # Add workload version in Juju status.
         self.unit.set_workload_version(self.version)
@@ -179,10 +184,10 @@ class FastAPIDemoCharm(ops.CharmBase):
         if not db_data:
             return {}
         env = {
-            "DEMO_SERVER_DB_HOST": db_data.get("db_host", None),
-            "DEMO_SERVER_DB_PORT": db_data.get("db_port", None),
-            "DEMO_SERVER_DB_USER": db_data.get("db_username", None),
-            "DEMO_SERVER_DB_PASSWORD": db_data.get("db_password", None),
+            'DEMO_SERVER_DB_HOST': db_data.get('db_host', None),
+            'DEMO_SERVER_DB_PORT': db_data.get('db_port', None),
+            'DEMO_SERVER_DB_USER': db_data.get('db_username', None),
+            'DEMO_SERVER_DB_PASSWORD': db_data.get('db_password', None),
         }
         return env
 
@@ -196,42 +201,42 @@ class FastAPIDemoCharm(ops.CharmBase):
         a dictionary. If no data is retrieved, the unit is set to waiting status and
         the program exits with a zero status code."""
         relations = self.database.fetch_relation_data()
-        logger.debug("Got following database data: %s", relations)
+        logger.debug('Got following database data: %s', relations)
         for data in relations.values():
             if not data:
                 continue
-            logger.info("New PSQL database endpoint is %s", data["endpoints"])
-            host, port = data["endpoints"].split(":")
+            logger.info('New PSQL database endpoint is %s', data['endpoints'])
+            host, port = data['endpoints'].split(':')
             db_data = {
-                "db_host": host,
-                "db_port": port,
-                "db_username": data["username"],
-                "db_password": data["password"],
+                'db_host': host,
+                'db_port': port,
+                'db_username': data['username'],
+                'db_password': data['password'],
             }
             return db_data
         return {}
 
     @property
     def _pebble_layer(self) -> ops.pebble.Layer:
-        """Return a Layer object representing a Pebble layer."""
-        command = " ".join(
+        """A Pebble layer for the FastAPI demo services."""
+        command = ' '.join(
             [
-                "uvicorn",
-                "api_demo_server.app:app",
-                "--host=0.0.0.0",
+                'uvicorn',
+                'api_demo_server.app:app',
+                '--host=0.0.0.0',
                 f"--port={self.config['server-port']}",
             ]
         )
         pebble_layer: ops.pebble.LayerDict = {
-            "summary": "FastAPI demo service",
-            "description": "pebble config layer for FastAPI demo server",
-            "services": {
+            'summary': 'FastAPI demo service',
+            'description': 'pebble config layer for FastAPI demo server',
+            'services': {
                 self.pebble_service_name: {
-                    "override": "replace",
-                    "summary": "fastapi demo",
-                    "command": command,
-                    "startup": "enabled",
-                    "environment": self.app_environment,
+                    'override': 'replace',
+                    'summary': 'fastapi demo',
+                    'command': command,
+                    'startup': 'enabled',
+                    'environment': self.app_environment,
                 }
             },
         }
@@ -246,13 +251,13 @@ class FastAPIDemoCharm(ops.CharmBase):
         # Catching Exception is not ideal, but we don't care much for the error here, and just
         # default to setting a blank version since there isn't much the admin can do!
         except Exception as e:
-            logger.warning("unable to get version from API: %s", str(e), exc_info=True)
-        return ""
+            logger.warning('unable to get version from API: %s', str(e), exc_info=True)
+        return ''
 
     def _request_version(self) -> str:
         """Helper for fetching the version from the running workload using the API."""
         resp = requests.get(f"http://localhost:{self.config['server-port']}/version", timeout=10)
-        return resp.json()["version"]
+        return resp.json()['version']
 
     @property
     def peers(self) -> Optional[ops.Relation]:
@@ -268,11 +273,11 @@ class FastAPIDemoCharm(ops.CharmBase):
         """Retrieve information from the peer data bucket instead of `StoredState`."""
         if not self.peers:
             return {}
-        data = self.peers.data[self.app].get(key, "")
+        data = self.peers.data[self.app].get(key, '')
         if not data:
             return {}
         return json.loads(data)
 
 
-if __name__ == "__main__":  # pragma: nocover
+if __name__ == '__main__':  # pragma: nocover
     ops.main(FastAPIDemoCharm)
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -21,15 +21,15 @@ def harness():
 def test_pebble_layer(
     monkeypatch: pytest.MonkeyPatch, harness: ops.testing.Harness[FastAPIDemoCharm]
 ):
-    monkeypatch.setattr(FastAPIDemoCharm, "version", "1.0.0")
+    monkeypatch.setattr(FastAPIDemoCharm, 'version', '1.0.0')
     # Expected plan after Pebble ready with default config
     expected_plan = {
-        "services": {
-            "fastapi-service": {
-                "override": "replace",
-                "summary": "fastapi demo",
-                "command": "uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000",
-                "startup": "enabled",
+        'services': {
+            'fastapi-service': {
+                'override': 'replace',
+                'summary': 'fastapi demo',
+                'command': 'uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000',
+                'startup': 'enabled',
                 # Since the environment is empty, Layer.to_dict() will not
                 # include it.
             }
@@ -37,16 +37,16 @@ def test_pebble_layer(
     }
 
     # Simulate the container coming up and emission of pebble-ready event
-    harness.container_pebble_ready("demo-server")
+    harness.container_pebble_ready('demo-server')
     harness.evaluate_status()
 
     # Get the plan now we've run PebbleReady
-    updated_plan = harness.get_container_pebble_plan("demo-server").to_dict()
-    service = harness.model.unit.get_container("demo-server").get_service("fastapi-service")
+    updated_plan = harness.get_container_pebble_plan('demo-server').to_dict()
+    service = harness.model.unit.get_container('demo-server').get_service('fastapi-service')
     # Check that we have the plan we expected:
     assert updated_plan == expected_plan
     # Check the service was started:
     assert service.is_running()
     # Ensure we set a BlockedStatus with appropriate message:
     assert isinstance(harness.model.unit.status, ops.BlockedStatus)
-    assert "Waiting for database" in harness.model.unit.status.message
+    assert 'Waiting for database' in harness.model.unit.status.message

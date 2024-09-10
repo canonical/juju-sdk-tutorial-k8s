dda3b55702d2ff91f6ff28368189320538b96093
James Garner <james.garner@canonical.com>
2024-09-10 15:12:46 +1200

    Minor improvements and ruff formatting

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -5,8 +5,9 @@ from pytest_operator.plugin import OpsTest
 async def get_address(ops_test: OpsTest, app_name: str, unit_num: int = 0) -> str:
     """Get the address for a the k8s service for an app."""
     status = await ops_test.model.get_status()
-    k8s_service_address = status["applications"][app_name].public_address
+    k8s_service_address = status['applications'][app_name].public_address
     return k8s_service_address
+
 
 def is_port_open(host: str, port: int) -> bool:
     """check if a port is opened in a particular host"""

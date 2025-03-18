8728219ce24a83fa5f36f38c23868b065f8064ef
Tiexin Guo <guotiexin@gmail.com>
2025-03-18 18:48:16 +0800

    chore: add unit test

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+import ops
+from ops import testing
+
+from charm import FastAPIDemoCharm
+
+
+def test_pebble_layer():
+    ctx = testing.Context(FastAPIDemoCharm)
+    container = testing.Container(name="demo-server", can_connect=True)
+    state_in = testing.State(
+        containers={container},
+        leader=True,
+    )
+    ctx.run(ctx.on.pebble_ready(container=container), state_in)
+
+    # Expected plan after Pebble ready with default config
+    expected_plan = {
+        "services": {
+            "fastapi-service": {
+                "override": "replace",
+                "summary": "fastapi demo",
+                "command": "uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000",
+                "startup": "enabled",
+                # Since the environment is empty, Layer.to_dict() will not
+                # include it.
+            }
+        }
+    }
+
+    # Check that we have the plan we expected:
+    assert container.plan == expected_plan
+    # Check the service was started:
+    assert (
+        container.service_statuses["fastapi-service"] == ops.pebble.ServiceStatus.ACTIVE
+    )
+
+
+def test_config_changed():
+    ctx = testing.Context(FastAPIDemoCharm)
+    container = testing.Container(name="demo-server", can_connect=True)
+    state_in = testing.State(
+        containers={container},
+        config={"server-port": 8080},
+        leader=True,
+    )
+    ctx.run(ctx.on.config_changed(), state_in)
+    assert (
+        "--port=8080"
+        in container.layers["fastapi_demo"].services["fastapi-service"].command
+    )

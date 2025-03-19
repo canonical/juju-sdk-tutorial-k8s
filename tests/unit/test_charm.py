d808d7ce1d4a037781cb3d19527e659c6005b928
Tiexin Guo <guotiexin@gmail.com>
2025-03-19 13:34:03 +0800

    chore: add ci

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -11,7 +11,7 @@ from charm import FastAPIDemoCharm
 
 def test_pebble_layer():
     ctx = testing.Context(FastAPIDemoCharm)
-    container = testing.Container(name='demo-server', can_connect=True)
+    container = testing.Container(name="demo-server", can_connect=True)
     state_in = testing.State(
         containers={container},
         leader=True,
@@ -19,12 +19,12 @@ def test_pebble_layer():
     state_out = ctx.run(ctx.on.pebble_ready(container), state_in)
     # Expected plan after Pebble ready with default config
     expected_plan = {
-        'services': {
-            'fastapi-service': {
-                'override': 'replace',
-                'summary': 'fastapi demo',
-                'command': 'uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000',
-                'startup': 'enabled',
+        "services": {
+            "fastapi-service": {
+                "override": "replace",
+                "summary": "fastapi demo",
+                "command": "uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000",
+                "startup": "enabled",
                 # Since the environment is empty, Layer.to_dict() will not
                 # include it.
             }
@@ -35,5 +35,5 @@ def test_pebble_layer():
     assert state_out.get_container(container.name).plan == expected_plan
     # Check the service was started:
     assert (
-        container.service_statuses['fastapi-service'] == ops.pebble.ServiceStatus.ACTIVE
+        container.service_statuses["fastapi-service"] == ops.pebble.ServiceStatus.ACTIVE
     )

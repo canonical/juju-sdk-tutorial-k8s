955ab5d1761b5a83039f25c9aa6b8edeca1f541e
Tiexin Guo <guotiexin@gmail.com>
2025-03-19 13:22:32 +0800

    test: add unit test

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -11,21 +11,20 @@ from charm import FastAPIDemoCharm
 
 def test_pebble_layer():
     ctx = testing.Context(FastAPIDemoCharm)
-    container = testing.Container(name="demo-server", can_connect=True)
+    container = testing.Container(name='demo-server', can_connect=True)
     state_in = testing.State(
         containers={container},
         leader=True,
     )
-    ctx.run(ctx.on.pebble_ready(container=container), state_in)
-
+    state_out = ctx.run(ctx.on.pebble_ready(container), state_in)
     # Expected plan after Pebble ready with default config
     expected_plan = {
-        "services": {
-            "fastapi-service": {
-                "override": "replace",
-                "summary": "fastapi demo",
-                "command": "uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000",
-                "startup": "enabled",
+        'services': {
+            'fastapi-service': {
+                'override': 'replace',
+                'summary': 'fastapi demo',
+                'command': 'uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000',
+                'startup': 'enabled',
                 # Since the environment is empty, Layer.to_dict() will not
                 # include it.
             }
@@ -33,23 +32,37 @@ def test_pebble_layer():
     }
 
     # Check that we have the plan we expected:
-    assert container.plan == expected_plan
+    assert state_out.get_container(container.name).plan == expected_plan
     # Check the service was started:
     assert (
-        container.service_statuses["fastapi-service"] == ops.pebble.ServiceStatus.ACTIVE
+        container.service_statuses['fastapi-service'] == ops.pebble.ServiceStatus.ACTIVE
     )
 
 
 def test_config_changed():
     ctx = testing.Context(FastAPIDemoCharm)
-    container = testing.Container(name="demo-server", can_connect=True)
+    container = testing.Container(name='demo-server', can_connect=True)
     state_in = testing.State(
         containers={container},
-        config={"server-port": 8080},
+        config={'server-port': 8080},
         leader=True,
     )
     ctx.run(ctx.on.config_changed(), state_in)
     assert (
-        "--port=8080"
-        in container.layers["fastapi_demo"].services["fastapi-service"].command
+        '--port=8080'
+        in container.layers['fastapi_demo'].services['fastapi-service'].command
+    )
+
+
+def test_config_changed_invalid_port():
+    ctx = testing.Context(FastAPIDemoCharm)
+    container = testing.Container(name='demo-server', can_connect=True)
+    state_in = testing.State(
+        containers={container},
+        config={'server-port': 22},
+        leader=True,
+    )
+    state_out = ctx.run(ctx.on.config_changed(), state_in)
+    assert state_out.unit_status == ops.BlockedStatus(
+        'Invalid port number, 22 is reserved for SSH'
     )

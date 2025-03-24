5083fd79f64eda38d89c57ab382252327066ae8c
Tiexin Guo <guotiexin@gmail.com>
2025-03-24 19:35:23 +0800

    chore: fix integration test ci

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -31,6 +31,6 @@ async def test_build_and_deploy(ops_test: OpsTest):
     await asyncio.gather(
         ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
         ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="idle", raise_on_blocked=False, timeout=120
+            apps=[APP_NAME], status="active", raise_on_blocked=False, timeout=300
         ),
     )
